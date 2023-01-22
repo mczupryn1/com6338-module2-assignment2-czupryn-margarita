0c7bf730581df03a961e2d9b7d42c9ee80eb5211
mczupryn1 <81737272+mczupryn1@users.noreply.github.com>
2023-01-22 17:51:20 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Your code here
+var full_year = parseInt(new Date().getFullYear())
+function greet() { 
+    var name_input = prompt("What is your name?") 
+    alert("Hello, " + name_input)
+    var userAge = prompt("How old are you, " + name_input + "?") 
+    userAge = parseInt(userAge)
+    if(confirm("If you have already had your birthday this year, click OK. If not, click Cancel.")) 
+    { 
+        alert((new Date().getFullYear() - userAge).toString()) 
+        } else {
+            userAge += 1
+            console.log(userAge)
+            alert((new Date().getFullYear() - userAge).toString()) 
+        }
+    }
